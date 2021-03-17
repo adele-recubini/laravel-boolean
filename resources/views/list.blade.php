5823c41829798331506db2a021c360ed692ed1dd
adele <adele.r87@gmail.com>
2021-03-17 17:28:40 +0100

    esercizio

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,7 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>parco macchine</title>
+  </head>
+  <body>
 
+<h1>database creato e importato da php my admin con una lista di macchine e loro proprietà </h1>
 @foreach ($cars as $car)
-  {{$car -> id}}
-  {{$car -> model}}
-  {{$car -> power}}
-  
+
+<div>
+  <br>
+  <span><strong>numero:</strong>{{$car -> id}}</span>
+  <span><strong>nome:</strong>{{$car -> name}}</span>
+  <span><strong>modello:</strong>{{$car -> model}}</span>
+  <span><strong>potenza:</strong>{{$car -> power}}</span>
+ </div>
+
+
 @endforeach
+</body>
+</html>
