b82cece9e9fd1f6741a0674ba1b3ce6c81f86f15
adele <adele.r87@gmail.com>
2021-03-17 17:31:57 +0100

    esercizio

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//ricordati di aggiungere nel controller la creazione del model
 use App\Car;
+//fine
 
 class ListController extends Controller
 {
